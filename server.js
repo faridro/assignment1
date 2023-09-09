8ab5afc00ff7a707cbd1373f467d6cfc14f9aaef
faridro <faridrostami2000@gmail.com>
2023-09-09 12:39:51 -0400

    "server.js update"

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
+/*********************************************************************************
+*  WEB322 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Farid Rostamilotfi    Student ID: 121274229   Date: 2023/09/09
+*
+*  Online (Cyclic) URL:     https://busy-puce-whale-shoe.cyclic.cloud
+*
+********************************************************************************/ 
+
+
 var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
